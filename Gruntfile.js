8b161c145e33f7e4e64e2bdd1928d45171807248
Ivan Krolo <ikrolo@gmail.com>
2013-06-20 11:31:26 -0400

    UTS-1236 Removed unused initConfig call

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,6 @@
 'use strict';
 
 module.exports = function(grunt) {
-	// Project configuration.
-	grunt.initConfig({
-		uptime_gadget_tasks: {
-			default_options: {
-				options: {
-				}
-	  		}
-		}
-	});
-
 	// Actually load this plugin's task(s).
 	grunt.loadTasks('tasks');
 
